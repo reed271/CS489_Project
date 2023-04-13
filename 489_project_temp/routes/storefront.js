ae403657a4fff912cc4e91be0f01610638827188
reed <reed.haves@wsu.edu>
2023-04-13 14:43:37 -0700

    Added item uploading; converted storefront to show products dynamically from DB

--- 489_project_temp/routes/storefront.js
+++ 489_project_temp/routes/storefront.js
@@ -1,12 +1,41 @@
 var express = require('express');
 var router = express.Router();
 const User = require('../models/User');
+const Product = require('../models/Product')
 
-router.get('/', function(req, res, next) {
+router.get('/', async function(req, res, next) {
     //console.log(req.body.username+" - "+req.body.password);
     const user = req.session.user
-    res.render("storefront", {user})
+    const products = await Product.findAll();
+    res.render("storefront", {user, products})
 });
+
+router.get('/sell_item', function(req, res, next) {
+    //console.log(req.body.username+" - "+req.body.password);
+    const user = req.session.user
+    res.render("sell_item", {user})
+});
+
+router.post('/sell_item/create', async function(req, res, next) {
+    //console.log(req.body.username+" - "+req.body.password);
+    const user = req.session.user
+    try {
+        await Product.create(
+          {
+            name: req.body.name,
+            desc: req.body.desc,
+            price: req.body.price,
+            owner: user.username
+          }
+    
+      )
+      res.redirect('/storefront')
+      } catch (error) {
+      res.redirect('/users') 
+      }
+    }
+);
+
 
 
 
--- 489_project_temp/routes/users.js
+++ 489_project_temp/routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const User = require('../models/User');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -8,6 +9,31 @@ router.get('/', function(req, res, next) {
 
 router.get('/test', function(req, res, next) {
   res.send('respond with a USER TEST');
+});
+
+router.get('/editprofile', function(req, res, next) {
+  res.render('edit_profile');
+});
+
+router.post('/editprofile/postchange', async function(req, res, next) {
+  const user = await User.findUser(req.session.user.username, req.session.user.password)
+  if(user!== null){
+    user.update({
+      address: req.body.address,
+      email: req.body.email
+    } )
+    await user.save()
+    req.session.user = user
+    res.render('view_profile', {user})
+  }else{
+    res.redirect("/?msg=fail")
+  }
+});
+
+router.get('/view_profile', function(req, res, next) {
+  //console.log(req.body.username+" - "+req.body.password);
+  const user = req.session.user
+  res.render("view_profile", {user})
 });
 
 module.exports = router;
