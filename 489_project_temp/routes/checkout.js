e34fdb8d0fd4cabbf02af31f230f3c5c15ff1dbd
Ben <shuyan.liu1@wsu.edu>
2023-04-21 23:44:44 -0700

    Add separate file for navbar
    This ejs file will be included in all other ejs files. Thus, changes to the navbar will be present in the entire project

--- 489_project_temp/routes/checkout.js
+++ 489_project_temp/routes/checkout.js
@@ -5,14 +5,31 @@ const Product = require('../models/Product');
 const { findProduct, findByPk } = require('../models/Product');
 const { findUser } = require('../models/User');
 
-router.get('/', async function(req, res, next) {
+router.get('/', async function (req, res, next) {
     const user = req.session.user
     const products = await Product.findAll();
     let total = 0;
     for (item of products) {
         total += item.price;
     }
-    res.render("checkout", {user, products, total})
+    res.render("checkout", { user, products, total })
 });
+
+// router.post('/createOrder', async function (req, res, next) {
+//     try {
+//         await Course.create(
+//             {
+//                 courseid: req.body.courseid,
+//                 coursename: req.body.coursename,
+//                 semester: req.body.semester,
+//                 coursedesc: req.body.coursedesc,
+//                 enrollnum: req.body.enrollnum
+//             }
+//         )
+//         res.redirect('/courses?msg=success&courseid=' + req.body.courseid)
+//     } catch (error) {
+//         res.redirect('/courses?msg=' + new URLSearchParams(error.toString()).toString() + '&courseid' + req.body.courseid)
+//     }
+// });
 
 module.exports = router;
