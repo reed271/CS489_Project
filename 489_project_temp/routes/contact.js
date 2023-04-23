9f651944f8da0769cd256eb79ed198c076c6c391
Ben <shuyan.liu1@wsu.edu>
2023-04-23 14:41:35 -0700

    Added Contact Routing

---
+++ 489_project_temp/routes/contact.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', async function (req, res, next) {
+    const user = req.session.user
+    res.render("contact", { user })
+});
+
+router.post('/submit', async function (req, res, next) {
+    res.redirect('/storefront')
+});
+
+module.exports = router;
