16a4b989abebad29b8875472eb8c91e289b8e2b3
Ben <shuyan.liu1@wsu.edu>
2023-04-23 16:04:22 -0700

    stuff

--- 489_project_temp/routes/checkout.js
+++ 489_project_temp/routes/checkout.js
@@ -7,7 +7,7 @@ const { findUser } = require('../models/User');
 
 router.get('/', async function (req, res, next) {
     const user = req.session.user
-    var products = await user.getProducts() //returns shopping cart contents
+    const products = await Product.findAll();
     let total = 0;
     for (item of products) {
         total += item.price;
@@ -15,21 +15,25 @@ router.get('/', async function (req, res, next) {
     res.render("checkout", { user, products, total })
 });
 
-// router.post('/createOrder', async function (req, res, next) {
-//     try {
-//         await Course.create(
-//             {
-//                 courseid: req.body.courseid,
-//                 coursename: req.body.coursename,
-//                 semester: req.body.semester,
-//                 coursedesc: req.body.coursedesc,
-//                 enrollnum: req.body.enrollnum
-//             }
-//         )
-//         res.redirect('/courses?msg=success&courseid=' + req.body.courseid)
-//     } catch (error) {
-//         res.redirect('/courses?msg=' + new URLSearchParams(error.toString()).toString() + '&courseid' + req.body.courseid)
-//     }
-// });
+router.post('/createOrder', async function (req, res, next) {
+    console.log(req.body)
+    /*
+    {
+    firstname: 'sdfas',
+    email: 'asdasd',
+    address: 'sadasd',
+    city: 'asd',
+    state: 'asdasd',
+    zip: 'asda',
+    cardname: 'adadasd',
+    cardnumber: 'asdasd',
+    expmonth: 'asdas',
+    expyear: 'asdasd',
+    cvv: 'asd',
+    sameadr: 'on'
+    }
+    */
+    res.redirect('/storefront')
+});
 
 module.exports = router;
