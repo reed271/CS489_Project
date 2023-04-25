5ecad80073bae7d5ca08017dc8bed1d968f0d8a4
Ben <shuyan.liu1@wsu.edu>
2023-04-24 18:18:02 -0700

    Revert "Add status router"
    This reverts commit cae93bdcc8d692877440742cc824859ea5c735c1.

--- 489_project_temp/app.js
+++ 489_project_temp/app.js
@@ -14,7 +14,6 @@ var registerRouter = require("./routes/register")
 var storefrontRouter = require("./routes/storefront");
 var checkoutRouter = require("./routes/checkout");
 var contactRouter = require("./routes/contact");
-var statusRouter = require("./routes/status");
 const Product = require('./models/Product');
 
 var app = express();
@@ -39,10 +38,10 @@ app.use(session({
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/register', registerRouter);
-app.use('/storefront', storefrontRouter);
-app.use('/checkout', checkoutRouter);
-app.use('/contact', contactRouter);
-app.use('/status', statusRouter);
+app.use('/storefront', storefrontRouter)
+app.use('/checkout', checkoutRouter)
+app.use('/contact', contactRouter)
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
